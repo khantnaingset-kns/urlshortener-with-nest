d91dc5890e6a86d2d3a6c00a66862ac144dd0552
Khant Naing Set <khantnaingset@outlook.com>
2023-05-28 07:58:11 +0630

    setup sonarcude

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { ComposerModule } from './modules/';
+import { JwtModule } from '@nestjs/jwt';
 import { LoggerModule } from '@app/logger';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 import { config } from './config';
 import { getMongoURI } from './app.utils';
 
@@ -30,7 +32,15 @@ import { getMongoURI } from './app.utils';
       load: [config],
     }),
     ComposerModule,
-    AuthModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('app.secret'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,24 +1,8 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { ApiUserModule } from './api-user/api-user.module';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [
-    ApiUserModule,
-    AuthModule,
-    PassportModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('app.secret'),
-        signOptions: { expiresIn: '1h' },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [ApiUserModule, AuthModule],
 })
 export class ComposerModule {}
